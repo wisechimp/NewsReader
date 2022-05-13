b09f2fd29ec30138dcc510b73d18fe6089b3b6bd
Stuart Hicks <stuart.hicks@gmail.com>
2022-05-13 09:09:50 +0200

    Starting setting up the parsing of the rss feed

--- App.tsx
+++ App.tsx
@@ -1,11 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { parse } from 'rss-to-json'
+import { AxiosRequestConfig } from 'axios'
+
+interface NewsStoryData {
+  title: string,
+  description: string,
+  link: string,
+  image: string,
+  pubDate: string 
+}
+
+interface NewsStoriesData {
+  title: string,
+  description: string,
+  items: Array<NewsStoryData>
+}
 
 export default function App() {
+  console.log('Hello!')
+  const newsStoriesData = () => {
+    return rssData = parse('https://www.svt.se/nyheter/rss.xml')
+      .then(rss => {console.log(JSON.stringify(rss, null, 3))}
+    }
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <FlatList 
+        data={newsStoriesData.items} 
+        renderItem={({item}) => <Text>item.title</Text>}
+      />
     </View>
   );
 }
