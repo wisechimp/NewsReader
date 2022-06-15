abaae9880edc742f94eb7e89d2a7fb1168bd9bca
shicks1080 <stuart.hicks@gmail.com>
2022-06-15 14:52:27 +0200

    refactor: Renaming the types files as .ts not .tsx

--- src/types/NavigationPropsType.tsx
+++
@@ -1,7 +0,0 @@
-import type { NativeStackScreenProps } from '@react-navigation/native-stack'
-
-import RootStackParamsList from '../types/RootStackParamsListType'
-
-type NavigationProps = NativeStackScreenProps<RootStackParamsList>
-
-export default NavigationProps
