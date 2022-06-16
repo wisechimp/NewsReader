04d356a5b1c206b1890a343c24d1e4f6a69320d5
shicks1080 <stuart.hicks@gmail.com>
2022-06-16 14:15:46 +0200

    feat: Creating a list of skeleton components for slow loading data
    For when the RSS feed takes some time to load.

--- src/components/LoadingSkeleton/LoadingSkeleton.tsx
+++ src/components/LoadingSkeleton/LoadingSkeleton.tsx
@@ -2,13 +2,23 @@ import React from 'react'
 import ContentLoader, { Rect } from "react-content-loader/native";
 
 interface LoadingSkeletonProps {
+  id: number,
   width: number,
   height: number
 }
 
 const LoadingSkeleton = ({ width, height }: LoadingSkeletonProps) => (
-  <ContentLoader viewBox={`0 0 ${width} ${height}`}>
-    <Rect x="20" y="17" rx="4" ry="4" width={`${width - 36}`} height={height} />
+  <ContentLoader 
+    viewBox={`0 0 ${width} ${height}`} 
+    backgroundColor='#b9b9b9'
+    speed={1}
+    style={{
+      borderBottomColor: '#000',
+      borderBottomWidth: 1,
+      height: 64
+    }}
+  >
+    <Rect x="20" y="0" rx="4" ry="4" width={`${width - 36}`} height={height} />
   </ContentLoader>
 )
 
--- src/components/NewsHeadlinesList/NewsHeadlinesList.tsx
+++ src/components/NewsHeadlinesList/NewsHeadlinesList.tsx
@@ -2,55 +2,65 @@ import React, { useEffect, useState } from 'react'
 import { View, FlatList, Pressable, StyleSheet } from 'react-native'
 
 import NewsHeadlinesListItem from '../NewsHeadlinesListItem/NewsHeadlinesListItem'
-import NavigationProps from '../../types/NavigationPropsType'
 import NewsStoriesData from '../../types/NewsStoriesDataType'
 import LoadingSkeleton from '../LoadingSkeleton/LoadingSkeleton'
+import { NativeStackScreenProps } from '@react-navigation/native-stack'
+import RootStackParamsList from '../../types/RootStackParamsListType'
+import { useNavigation } from '@react-navigation/native'
 
 interface NewsHeadlinesListProps {
-  navigation: NavigationProps,
-  data: NewsStoriesData
+  data: NewsStoriesData,
+  navigation: NativeStackScreenProps<RootStackParamsList, 'Headlines'>
 }
 
-const NewsHeadlinesList = ({ data, navigation }: NewsHeadlinesListProps) => {
-
+const NewsHeadlinesList = ({ data }: NewsHeadlinesListProps) => {
   console.log(data.items)
   const [ isLoading, setIsLoading ] = useState(true)
   const [ windowWidth, setWindowWidth ] = useState(456)
+  const navigation = useNavigation()
+  const skeletonData: Array<number> = []
 
   const waitXSeconds = (time: number) => {
     return new Promise(resolve => setTimeout(resolve, time))
   }
 
+  const generateSkeletonData = (int: number) => {
+    for (let i: number = 0; i < int; i++) {  
+      skeletonData.push(i)
+    }
+  }
+  generateSkeletonData(12)
+
   useEffect(() => {
     // setWindowWidth(window.innerWidth)
     waitXSeconds(2000).then(() => setIsLoading(false))
   }, [])
+
   return (
-    
       <View>
         { isLoading
-          ? <LoadingSkeleton
-              width={windowWidth}
-              height={20}
-          />
+          ? <FlatList
+              data={skeletonData}
+              renderItem={({item}) =>
+                <LoadingSkeleton
+                  id={item}
+                  width={windowWidth}
+                  height={20}
+                 />
+              }
+            />
           : <FlatList 
-            data={data.items} 
-            renderItem={({item}) => 
-              <Pressable onPress={() => navigation.navigate('NewsArticle', { newsStory: {item}})}>
-                <NewsHeadlinesListItem title={item.title}/>
-              </Pressable>
-            }
-          />
+              data={data.items} 
+              renderItem={({item}) => 
+                <Pressable onPress={() => navigation.navigate('NewsArticle', { newsStory: {item}})}>
+                  <NewsHeadlinesListItem title={item.title}/>
+                </Pressable>
+              }
+            />
         }
       </View>
     
   )
 }
-
-const styles = StyleSheet.create({
-  skeletonList: {
-    margin: 0
-  }
-})
 
 export default NewsHeadlinesList
